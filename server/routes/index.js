589537f36df596f0ae78b256705036f90e46d1d1
Jan Skowronski <crianonim@gmail.com>
2019-05-22 18:45:03 +0100

    json endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,10 @@ router.get('/data', async (req, res) => {
   let data = await lib.readDir(path.join(__dirname, "..", "abulafa"));
   res.send(`let data=${JSON.stringify(data, null, 1)}`)
 })
+router.get('/json', async(req, res) => {
+  let data = await lib.readDir(path.join(__dirname, "..", "abulafa"));
+  res.json(data);
+});
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
